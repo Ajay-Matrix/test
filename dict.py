af7cc19fd92df66b952b1445393e57ebea7262b2
Ajay-Matrix <ajayabraham2003@gmail.com>
2025-08-20 13:11:23 +0530

    removed x

--- dict.py
+++ dict.py
@@ -1,14 +1,7 @@
-x={
-    'name': 'ammu',
-    'age': 25,
-    'city': 'kerala',
-    'email':'ajayabraham2003@gmail.com'
-    
-}
+
 print(x)
 print(type(x))
 x=dict(name='ammu', age=25, city='kozhikode',)
 print(x)
 a=x['name']
 print(a)
-b=
