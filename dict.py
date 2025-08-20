ebf689a28cb42d6c8584648acc6a0068d9d1ebbc
Ajay-Matrix <ajayabraham2003@gmail.com>
2025-08-20 13:16:31 +0530

    added ajmal

--- dict.py
+++ dict.py
@@ -1,7 +1,14 @@
-
+x={
+    'name': 'ammu',
+    'age': 25,
+    'city': 'kerala',
+    'email':'ajayabraham2003@gmail.com'
+    
+}
 print(x)
 print(type(x))
 x=dict(name='ammu', age=25, city='kozhikode',)
 print(x)
 a=x['name']
 print(a)
+b='ajmal'
